06f70540f195834f2bc639df02b6177ca38cfbb8
ilyana19 <aries_2152@hotmail.com>
2018-08-31 10:59:46 -0400

    finished dictionary file

---
+++ dictionary.rb
@@ -0,0 +1,29 @@
+class Dictionary
+  attr_reader :entries
+
+  def initialize
+    @entries = {}
+  end
+
+  def add(entry)
+    if entry.is_a?(Hash)
+      @entries.merge!(entry)
+    else
+      @entries.merge!({entry => nil})
+    end
+  end
+
+  def keywords
+    @entries.keys.sort
+  end
+
+  def find(entry)
+    @entries.select do |key, value|
+      key.include?(entry)
+    end
+  end
+
+  def include?(entry)
+    @entries.has_key?(entry)
+  end
+end
